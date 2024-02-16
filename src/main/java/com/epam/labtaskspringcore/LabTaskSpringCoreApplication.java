0200a48e3e3f0efd652f42e29328d434702a4a5a
Guram Salia <guramsalia@gmail.com>
2024-02-16 23:06:22 +0400

    initial commit

---
+++ src/main/java/com/epam/labtaskspringcore/LabTaskSpringCoreApplication.java
@@ -0,0 +1,13 @@
+package com.epam.labtaskspringcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabTaskSpringCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabTaskSpringCoreApplication.class, args);
+	}
+
+}
