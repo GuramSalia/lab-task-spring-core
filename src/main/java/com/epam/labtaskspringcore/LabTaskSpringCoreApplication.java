4436339afa3a25e56f04918f7a56b878fad22d3f
Guram Salia <guramsalia@gmail.com>
2024-02-21 01:31:56 +0400

    feat: update TrainingService, restrict creating training if Trainer has diff. specialization

--- src/main/java/com/epam/labtaskspringcore/LabTaskSpringCoreApplication.java
+++ src/main/java/com/epam/labtaskspringcore/LabTaskSpringCoreApplication.java
@@ -3,12 +3,15 @@ package com.epam.labtaskspringcore;
 import com.epam.labtaskspringcore.config.ApplicationContextProvider;
 import com.epam.labtaskspringcore.config.InMemoryStorage;
 import com.epam.labtaskspringcore.model.Trainer;
+import com.epam.labtaskspringcore.model.Training;
 import com.epam.labtaskspringcore.model.TrainingType;
 import com.epam.labtaskspringcore.service.TraineeService;
 import com.epam.labtaskspringcore.service.TrainerService;
 import com.epam.labtaskspringcore.service.TrainingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class LabTaskSpringCoreApplication {
@@ -50,5 +53,28 @@ public class LabTaskSpringCoreApplication {
         trainer4.setSpecialization(TrainingType.YOGA);
         System.out.println(trainerService.create(trainer4));
         System.out.println("\n\t end of trainer 4 \n");
+
+        System.out.println("\n \ttraining 3 \n");
+        Training training3 = new Training();
+        training3.setTrainingId(3);
+        training3.setTrainerId(3);
+        training3.setTraineeId(2);
+        training3.setName("personal training");
+        training3.setType(TrainingType.PERSONAL);
+        training3.setDurationInMinutes(30);
+        System.out.println(trainingService.create(training3));
+        System.out.println("\n\tend of training 3 \n");
+
+
+        System.out.println("\n \ttraining 4 \n");
+        Training training4 = new Training();
+        training4.setTrainingId(4);
+        training4.setTrainerId(3);
+        training4.setTraineeId(2);
+        training4.setName("Yoga training");
+        training4.setType(TrainingType.YOGA);
+        training4.setDurationInMinutes(30);
+        System.out.println(trainingService.create(training4));
+        System.out.println("\n\tend of training 4 \n");
     }
 }
--- src/main/java/com/epam/labtaskspringcore/service/TrainingService.java
+++ src/main/java/com/epam/labtaskspringcore/service/TrainingService.java
@@ -1,21 +1,33 @@
 package com.epam.labtaskspringcore.service;
 
-import com.epam.labtaskspringcore.dao.TrainingDAOImpl;
+import com.epam.labtaskspringcore.dao.TrainerDAO;
+import com.epam.labtaskspringcore.dao.TrainingDAO;
 import com.epam.labtaskspringcore.model.Training;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.epam.labtaskspringcore.model.TrainingType;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TrainingService {
-    private TrainingDAOImpl trainingDAO;
+    private TrainingDAO trainingDAO;
+    private TrainerDAO trainerDAO;
 
-    public TrainingService(TrainingDAOImpl trainingDAO) {
+    public TrainingService(TrainingDAO trainingDAO, TrainerDAO trainerDAO) {
         this.trainingDAO = trainingDAO;
+        this.trainerDAO = trainerDAO;
     }
 
     public Training create(Training training) {
-        trainingDAO.create(training);
-        return trainingDAO.getById(training.getTrainingId());
+        TrainingType trainingTypeOfTraining = training.getType();
+        TrainingType trainingTypeOfTrainer = trainerDAO.getById(training.getTrainerId()).getSpecialization();
+
+        if (!trainingTypeOfTraining.equals(trainingTypeOfTrainer)) {
+            System.out.println("cannot create training, because the trainer has a different specialization");
+            return null;
+        } else {
+            trainingDAO.create(training);
+            return trainingDAO.getById(training.getTrainingId());
+        }
+
     }
 
     public Training getById(int id) {
